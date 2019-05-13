077cb70b131c87b0e54d0b77b2fcec24e71e312f
BrewingF1997 <675694706@qq.com>
2019-05-13 16:28:23 +0800

    Site updated: 2019-05-13 16:28:19

---
+++ js/src/lifephotos/lifephotos.js
@@ -0,0 +1,16 @@
+window.onload=function(){//当页面加载完成时执行操作
+    var objBox=document.getElementById("box");//获取一个对象  id
+//            alert(objBox.innerHTML); //看看对象成功与否
+    var str="<div class='pic'>";
+        for(var i=1;i<=16;++i){
+            if(i<10){
+                str+="<img src='/images/gflife/"+i+".jpg' />";
+            }else if(i<100){
+                str+="<img src='/images/gflife/"+i+".jpg' />";
+            }else{
+                str+="<img src='/images/gflife/"+i+".jpg' />";
+            }
+        }
+        str+="</div>";
+        objBox.innerHTML=str;//将对象中的内容修改
+    }
